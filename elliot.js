87e6cba0b910484edbf295f015026b54f2ebd0f6
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2012-07-13 10:28:12 +0200

    Rewrote the app to be extending classes rathern than one biggie

--- elliot.js
+++ elliot.js
@@ -1,28 +1,111 @@
-function Elliot (canvas_id, config) {
-	// Canvas and context
-	this.canvas = document.getElementById(canvas_id);
-	this.context = this.canvas.getContext('2d');
+/* Simple JavaScript Inheritance
+ * By John Resig http://ejohn.org/
+ * MIT Licensed.
+ */
+(function(){
+  var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
 
-	// Bar data array
-	this.barData = [{'title': '', 'value': this.canvas.height}];
+  // The base Class implementation (does nothing)
+  this.Class = function(){};
+ 
+  // Create a new Class that inherits from this class
+  Class.extend = function(prop) {
+    var _super = this.prototype;
+   
+    // Instantiate a base class (but only create the instance,
+    // don't run the init constructor)
+    initializing = true;
+    var prototype = new this();
+    initializing = false;
+   
+    // Copy the properties over onto the new prototype
+    for (var name in prop) {
+      // Check if we're overwriting an existing function
+      prototype[name] = typeof prop[name] == "function" &&
+        typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+        (function(name, fn){
+          return function() {
+            var tmp = this._super;
+           
+            // Add a new ._super() method that is the same method
+            // but on the super-class
+            this._super = _super[name];
+           
+            // The method only need to be bound temporarily, so we
+            // remove it when we're done executing
+            var ret = fn.apply(this, arguments);       
+            this._super = tmp;
+           
+            return ret;
+          };
+        })(name, prop[name]) :
+        prop[name];
+    }
+   
+    // The dummy class constructor
+    function Class() {
+      // All construction is actually done in the init method
+      if ( !initializing && this.init )
+        this.init.apply(this, arguments);
+    }
+   
+    // Populate our constructed prototype object
+    Class.prototype = prototype;
+   
+    // Enforce the constructor to be what we expect
+    Class.prototype.constructor = Class;
 
-	// Logging functions
-	this.logDebug = function (message) { console.log('DEBUG - ' + message); };
-	this.logError = function (message) { console.log('ERROR - ' + message); };
-	this.logInfo = function (message) { console.log('INFO - ' + message); };
-	this.logWarning = function (message) { console.log('WARN - ' + message); };
+    // And make this class extendable
+    Class.extend = arguments.callee;
+   
+    return Class;
+  };
+})("Class");
 
-	// Draw the background color
-	this.drawBackground = function () {
+/*
+ * Ellit graphing library implementation
+ *
+ */
+var Elliot = Class.extend({
+	init: function (canvas_id, config) {
+		// Canvas and context
+		this.canvas = document.getElementById(canvas_id);
+		this.context = this.canvas.getContext('2d');
+
+		// Check that the user has defined width and height on the canvas
+		if (isNaN(this.canvas.width) || isNaN(this.canvas.height))
+			this.logError('You must set both width and height on your canvas');
+		else
+			this.logDebug('Canvas dimensions set to ' + this.canvas.width + 'x' + this.canvas.height);
+	},
+	logDebug: function (message) { console.log('DEBUG - ' + message); },
+	logError: function (message) { console.log('ERROR - ' + message); },
+	logInfo: function (message) { console.log('INFO - ' + message); },
+	logWarning: function (message) { console.log('WARN - ' + message); },
+	drawBackground: function () {
 		this.context.fillStyle = config['general']['background'];
 		this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
-	};
+	}
+});
 
-	this.addBarGraphData = function (barData) {
+var ElliotBarGraph = Elliot.extend({
+	init: function (canvas_id, config) {
+		this._super(canvas_id, config);
+
+		// Bar data array
+		this.barData = [{'title': '', 'value': this.canvas.height}];
+
+		// Update the graph continously
+		this.drawInterval = setInterval((function (self) {
+			return function () {
+				self.drawBarGraph();
+			};
+		})(this), 50);
+	},
+	addBarGraphData: function (barData) {
 		this.barData = barData;
-	};
-
-	this.drawBarGraph = function () {
+	},
+	drawBarGraph: function () {
 		this.logInfo('Drawing bar graph');
 
 		// Fill the background
@@ -50,33 +133,5 @@ function Elliot (canvas_id, config) {
 			this.context.fillRect(from_x, from_y, bar_width, -(this.canvas.height - this.barData[i]['value']));
 		}
 		this.context.restore();
-	};
-
-	// Check that the user has defined width and height on the canvas
-	if (isNaN(this.canvas.width) || isNaN(this.canvas.height))
-		this.logError('You must set both width and height on your canvas');
-	else
-		this.logDebug('Canvas dimensions set to ' + this.canvas.width + 'x' + this.canvas.height);
-
-	// Add data to the graph
-	this.dataInterval = setInterval((function (self) {
-		return function () {
-			barData = [
-				{'title': 'Apple', 'value': Math.floor((Math.random()*500)+1)},
-				{'title': 'Pear', 'value': Math.floor((Math.random()*500)+1)},
-				{'title': 'Strawberries', 'value': Math.floor((Math.random()*500)+1)},
-				{'title': 'Melon', 'value': Math.floor((Math.random()*500)+1)},
-				{'title': 'Dragon fruit', 'value': Math.floor((Math.random()*500)+1)},
-				{'title': 'Papaya', 'value': Math.floor((Math.random()*500)+1)}
-				];
-			self.addBarGraphData(barData);
-		};
-	})(this), 1000);
-
-	// Update the graph continously
-	this.drawInterval = setInterval((function (self) {
-		return function () {
-			self.drawBarGraph();
-		};
-	})(this), 50);
-}
+	},
+});
